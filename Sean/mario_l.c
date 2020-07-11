59dae3c8c547323150fca668582dd1e7140b34e1
iamernie8199 <iamernie8199@gmail.com>
2020-07-11 13:43:10 +0800

    Create mario_l.c

---
+++ Sean/mario_l.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int h = 0;
+    while (h > 8 || h < 1) 
+    { 
+        h = get_int("Height: ");
+    } 
+    for (int i = 1; i <= h; i++)
+    {
+        for (int j = h; j > i; j--)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
