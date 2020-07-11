f062b15de297b560c9f7f2e4c4a358650e2625ad
iamernie8199 <iamernie8199@gmail.com>
2020-07-11 13:43:17 +0800

    Create cash.c

---
+++ Sean/cash.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
+
+void cal(int *money, int *change, int coin)
+{
+    int tmp = *money / coin;
+    *money = *money - tmp * coin;
+    *change = *change + tmp;
+}
+
+int main(void)
+{
+    float c = -1.0;
+    while (c < 0) 
+    { 
+        c = get_float("Change owed: ");
+    }
+    int coin = 0;
+    int tmp = 0;
+    int cents = round(c * 100);
+    
+    cal(&cents, &coin, 25);
+    cal(&cents, &coin, 10);
+    cal(&cents, &coin, 5);
+    cal(&cents, &coin, 1);
+    printf("%d\n", coin);
+}
